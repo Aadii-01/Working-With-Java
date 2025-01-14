085d7be17b0b767a21e096454e129a2e8da8b4f9
Aadii-01 <blazehere95@gmail.com>
2025-01-14 19:12:09 +0530

    Control Statements

--- Introduction/ControlStatements.java
+++ Introduction/ControlStatements.java
@@ -16,7 +16,14 @@ public class ControlStatements {
             if(prn.equals("1") || (prn.equals("2"))) {
                 System.out.println("Hi " + name);
             }
+        } else {
+            System.out.println("You're Welcomed!");
         }
+
+        System.out.println("Enter any random number: ");
+        int randInt = scanner.nextInt();
+        int score = (randInt > 9)? 0 : 1;
+        System.out.printf("Your score is : %d", score);
 
         System.out.println("Outside of the condition");
         scanner.close();
